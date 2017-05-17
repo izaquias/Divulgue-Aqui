28583819b9539cd905f092a6874938a431dbcb68
izaquias cavalcante <izaquiascavalcante@gmail.com>
2017-05-17 13:09:24 -0300

    Adicionado os posicionamentos e telas, estórias do user e outros

---
+++ Logado.lua
@@ -0,0 +1,82 @@
+local widget =  require ("widget")
+local composer = require ("composer")
+local scene = composer.newScene()
+--local Publicacao = require("Publicacao")
+--local usuarios = require("ListarUsuarios")
+
+
+
+
+local ButtonScheduling -- botao de agendamento
+local ButtonProfile
+local ButtonPublicacao
+local ButtonListar
+
+function scene:create(event)
+
+	local grupoCena = self.view 
+
+	ButtonScheduling = widget.newButton( {label="Agenda", x = display.contentWidth/2 + 5, y =display.contentHeight/2 - 150, shape = "roundedRect" , fillColor = { default={1,1,0,1}, over={1,0.1,0.7,0.4}}, onPress = realizarAgendamento } )
+	grupoCena:insert(ButtonScheduling)
+
+	ButtonProfile = widget.newButton( {label="Perfil", x = display.contentWidth/2 + 5, y =display.contentHeight/2 - 50, shape = "roundedRect" , fillColor = { default={1,1,0,1}, over={1,0.1,0.7,0.4}}, onPress = visualizarPerfil } )
+	grupoCena:insert(ButtonProfile)
+
+	ButtonPublicacao = widget.newButton( {label="Publicação", x = display.contentWidth/2 + 5, y =display.contentHeight/2 - 100, shape = "Rect" , fillColor = { default={1,1,0,1}, over={1,0.1,0.7,0.4}}, onPress = realizarPublicacao})
+	grupoCena:insert( ButtonPublicacao)
+
+	ButtonListar = widget.newButton( {label = "Usuários", x = display.contentWidth/2 + 5, y = display.contentHeight/2, shape = "Rect" , fillColor = { default={1,1,0,1}, over={1,0.1,0.7,0.4}}, onPress = visualizarUsuarios} )
+    grupoCena:insert(ButtonListar)
+end
+
+function  realizarAgendamento(event) -- toque no botao Agenda 
+	-- vai para a pagina de agendamento
+	if event.phase == "began" then
+		composer.gotoScene("Agendamento")
+	end
+	--if event.phase == "ended" then
+	--	print("xau")
+	--end
+end
+
+function  visualizarPerfil(event) -- toque no botao Perfil 
+	-- vai para a pagina de perfil
+	if event.phase == "began" then
+		composer.gotoScene("Perfil")
+	end
+	--if event.phase == "ended" then
+	--	print("xau")
+	--end
+end
+
+function realizarPublicacao(event)
+	if event.phase == "began" then
+		composer.gotoScene("Publicacao")
+	end
+	--if event.phase == "ended" then
+	--	print("xau")
+	--end
+end
+
+function visualizarUsuarios(event)
+	if event.phase == "began" then
+		composer.gotoScene( "ListarUsuarios" )
+	end
+end
+
+function scene:show(event)
+end
+
+function scene:hide(event)
+end
+
+
+function scene:destroy(event)
+end
+
+scene:addEventListener( "create", scene ) -- adiciona o evento da funcao de criar 
+scene:addEventListener( "show", scene ) -- adiciona o evento da funcao de entre 
+scene:addEventListener( "hide", scene ) -- adiciona o evento da funcao de sair
+scene:addEventListener( "destroy", scene )-- adiciona o evento da funcao de destruir 
+
+return scene
--- Login.lua
+++ Login.lua
@@ -2,6 +2,13 @@ local widget =  require ("widget") -- para os botoes
 local composer = require ("composer") -- para as telas
 local scene = composer.newScene()
 
+-----------------------conectar ao banco de dados ------------------------
+local sqlite3 = require( "sqlite3" )
+ 
+local path = system.pathForFile( "data.db", system.DocumentsDirectory )
+local db = sqlite3.open( path )
+--------------------------------------------------------------------------
+
 local LabelUser
 local LabelPassword
 local TxtUserName
@@ -13,32 +20,51 @@ local storeID -- armazenar id
 
 function scene:create(event)
 	
-	local grupoDeSena = self.view 	
+	local grupoCena = self.view 	
 
 	LabelUser = display.newText({text="UserName",x=display.contentWidth/2,y=display.contentHeight/2 - 23})
 	LabelUser:setFillColor(0,1,0)
-	grupoDeSena:insert(LabelUser)
+	grupoCena:insert(LabelUser)
 
 	LabelPassword = display.newText({text="Password",x=display.contentWidth/2,y=display.contentHeight/2 + 25})
 	LabelPassword:setFillColor(0,1,0)
-	grupoDeSena:insert(LabelPassword)
 
-	Buttonlogin = widget.newButton( {label="login", x = display.contentWidth/2, y = display.contentHeight/2 + 80, onPress = touchOnButtonLogin} )
-	grupoDeSena:insert(Buttonlogin)
+	grupoCena:insert(LabelPassword)
 
-	ButtonSingIn = widget.newButton( {label="Register", x = display.contentWidth/2 + 100, y = display.contentHeight/2 + 150, onPress = touchOnButtonSingIn} )
-	grupoDeSena:insert(ButtonSingIn)
+	Buttonlogin = widget.newButton( {label="Login", x = display.contentWidth/2 -50, y = display.contentHeight/2 + 80, onPress = touchOnButtonLogin} )
+	grupoCena:insert(Buttonlogin)
+
+	ButtonSingIn = widget.newButton( {label="Cadastre-se", x = display.contentWidth/2 + 30, y = display.contentHeight/2 + 80, onPress = registrarUsuario} )
+	grupoCena:insert(ButtonSingIn)
 end
 
 function StoreID(id)
-
-	ID = id	
+	return id
 end
 
 function touchOnButtonLogin(event) -- toque no botao de login
+
+	if event.phase == "began" then
+		for row in db:nrows("SELECT * FROM usuario") do
+    	print( "Row id " .. row.id )
+
+    		if row.nome == TxtUserName.text or row.email == TxtUserName.text then
+    			if row.senha == TxtPassword.text then
+    				--print( "Row id " .. row.id )
+    			StoreID(row.id)
+    			composer.gotoScene("Logado")
+    			end
+    		end
+    	end    	
+	end
 end
 
-function  touchOnButtonSingIn(event) -- toque no botao sing in 
+function  registrarUsuario(event) -- toque no botao sing in 
+	-- vai para a pagina de cadastro
+	if event.phase == "began" then
+		composer.gotoScene("CadastrarUsuario")
+	end
+
 end
 
 function scene:show(event) 
@@ -46,6 +72,7 @@ function scene:show(event)
 	if event.phase == "did" then
 		TxtUserName = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 25 )
 		TxtPassword = native.newTextField(display.contentWidth/2, display.contentHeight/2 + 50, 150, 25 )
+		TxtPassword.isSecure = true
 	end
 end
 
@@ -65,3 +92,23 @@ scene:addEventListener( "hide", scene ) -- adiciona o evento da funcao de sair
 scene:addEventListener( "destroy", scene )-- adiciona o evento da funcao de destruir 
 
 return scene
+
+
+
+
+
+
+
+
+
+
+
+--[[
+local email = "username@domain.com"
+ 
+if ( email:match("[A-Za-z0-9%.%%%+%-]+@[A-Za-z0-9%.%%%+%-]+%.%w%w%w?%w?") ) then
+   print( email.." IS formatted properly." )
+else
+   print( email.." is NOT formatted properly." )
+end
+]]---
