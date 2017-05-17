f154f00aaf24552ed4f0cc4bad7f78bf89bf1829
izaquias cavalcante <izaquiascavalcante@gmail.com>
2017-05-17 12:10:58 -0300

    Testando o repositório local e mandando 1º commit

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
---local composer = require ("composer")
---composer.gotoScene("login")
+display.setStatusBar( display.HiddenStatusBar )-- ocultar o status bar
+
+-- O objeto principal que a API Storyboard é manipular um objeto "cena".
+-- https://coronalabs.com/blog/2012/03/27/storyboard-scene-events-explained/
+--https://docs.coronalabs.com/guide/system/composer/index.html
+local composer = require ("composer")
+composer.gotoScene("login")
 
 
 --[[
