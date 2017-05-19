0c318cc2bd4bad133921205629be8f701af843fd
Anderson Felipe <andersonfseabra3@gmail.com>
2017-05-19 15:14:54 -0300

    inserindo no banco com o rest

--- web-service/src/java/WS/webService.java
+++ web-service/src/java/WS/webService.java
@@ -5,19 +5,27 @@
  */
 package WS;
 
+import ModelBeans.BeansFeedDeNoticia;
 import ModelBeans.BeansOrgao;
 import ModelBeans.BeansUsuario;
+import ModelDao.FeedDeNoticiaDao;
 import ModelDao.OrgaoDao;
 import ModelDao.UsuarioDao;
 import com.google.gson.Gson;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * REST Web Service
@@ -45,41 +53,97 @@ public class webService {
     public String getJson() {
        return "meu web service";
     }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    ///////////////////////////USUARIO///////////////////////////////////
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("usuario/insert")
-    public String insertUsuario(){
+    public String insertUsuario(String json){
         
         UsuarioDao u = new UsuarioDao();
         BeansUsuario mod = new BeansUsuario();
         
-        mod.setNome("marcela");
-        mod.setEmail("marcela@marcela.com");
-        mod.setSenha("123");
-        u.salvar(mod);
+         JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
         
-        Gson g = new Gson();
-        return g.toJson(mod);
+        String nome;
+        String email;
+        String senha;
+	
+        
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+            
+            nome = (String) jsonObject.get("nome");
+            email = (String)jsonObject.get("email");
+            senha = (String) jsonObject.get("senha");
+            
+            
+            mod.setNome(nome);
+            mod.setEmail(email);
+            mod.setSenha(senha);
+            u.salvar(mod);           
+           
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
     }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("orgao/insert")
-    public String insertOrgao(){
+    ///////////////////////////ORGAO///////////////////////////////////
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("orgao/inserir")
+    public String insertOrgao(String json){
     
         OrgaoDao o = new OrgaoDao();
         BeansOrgao mod = new BeansOrgao();
         
-        mod.setNome("compesa");
-        mod.setSenha("123");
-        o.salvar(mod);
+        JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
+        
+        String nome;
+        String senha;
+	
+        
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+            
+            nome = (String) jsonObject.get("nome");
+            senha = (String) jsonObject.get("senha");
+            
+            
+            mod.setNome(nome);
+            mod.setSenha(senha);
+            o.salvar(mod);           
+           
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    ///////////////////////////FEED///////////////////////////////////
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("feed/insert")
+    public String insertFeed(){
+    
+        FeedDeNoticiaDao f = new FeedDeNoticiaDao();
+        BeansFeedDeNoticia mod = new BeansFeedDeNoticia();
+        
+        mod.setLocalidade("rua C");
+        mod.setHora(null);
+        mod.setDescricao("rua com buraco");
+        mod.setCategoria("pavimentação");
+        mod.setIdUsuario("1");
+        f.salvar(mod);
+        
         
         Gson g = new Gson();
         return g.toJson(mod);
-        
     }
-
     /**
      * PUT method for updating or creating an instance of webService
      * @param content representation for the resource
---
+++ web-service/src/java/teste/main.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.JOptionPane;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author ander
+ */
+public class main {
+    
+    public static void main(String[] args) {
+         
+    ///////////////////////////ORGAO-INSERIR/////////////////////////////////// 
+    /*  
+        String nome = "celpe";
+        String senha = "123";
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("senha", senha);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-service/webresources/webService/orgao/inserir");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+    */    
+            
+    ///////////////////////////USUARIO-INSERIR///////////////////////////////////         
+    /*        
+        String nome = "celpe";
+        String email = "celp@celp.com";
+        String senha = "123";
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("email", email);
+        jsonObject.put("senha", senha);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-service/webresources/webService/usuario/insert");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+    */        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    }
+    
+}
