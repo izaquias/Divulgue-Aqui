bf8115bc92110ff71bea9bf51b637c5ab67ccb89
Anderson Felipe <andersonfseabra3@gmail.com>
2017-05-29 20:32:38 -0300

    transferido o projeto para o tipo de projeto maven

---
+++ WebServiceMavenDivulgueAqui/src/main/java/WS/webService.java
@@ -0,0 +1,243 @@
+
+package WS;
+
+import ModelBeans.BeansFeedDeNoticia;
+import ModelBeans.BeansOrgao;
+import ModelBeans.BeansUsuario;
+import ModelDao.FeedDeNoticiaDao;
+import ModelDao.OrgaoDao;
+import ModelDao.UsuarioDao;
+import com.google.gson.Gson;
+//import com.google.gson.Gson;
+import java.sql.Date;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.json.simple.JSONObject;
+//import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+
+/**
+ * REST Web Service
+ *
+ * @author ander
+ */
+@Path("webService")
+public class webService {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public webService() {
+    }
+
+    /**
+     * Retrieves representation of an instance of WS.webService
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+       return "meu web service";
+    }
+    ///////////////////////////USUARIO///////////////////////////////////
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("usuario/insert")
+    public String insertUsuario(String json){
+        
+        UsuarioDao u = new UsuarioDao();
+        BeansUsuario mod = new BeansUsuario();
+        
+        JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
+        
+        String nome;
+        String email;
+        String senha;
+	
+        
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+      
+        nome = (String) jsonObject.get("nome");
+        email = (String)jsonObject.get("email");
+        senha = (String) jsonObject.get("senha");
+        mod.setNome(nome);
+        mod.setEmail(email);
+        mod.setSenha(senha);
+        u.salvar(mod);
+          } catch (org.json.simple.parser.ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("usuario/recuperarPorId")
+    public String recuperarUsuarioPorId(@QueryParam("id") Long json){
+        
+        UsuarioDao u = new UsuarioDao();
+        BeansUsuario mod = new BeansUsuario();
+      
+        mod.setPesquisarPorId(json);
+        mod = u.buscarPorId(mod);
+        
+        Gson g = new Gson();
+        return g.toJson(mod);
+    }
+    
+     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("usuario/recuperarNom")
+    public String recuperarUsuarioPorNome(@QueryParam("nome") String json){
+        
+        UsuarioDao u = new UsuarioDao();
+        BeansUsuario mod = new BeansUsuario();
+      
+        mod.setPesquisarPorNome(json);
+        mod = u.buscarPorNome(mod);
+        
+        Gson g = new Gson();
+        return g.toJson(mod);
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("usuario/update")
+    public String updateUsuario(String json){
+        
+        UsuarioDao u = new UsuarioDao();
+        BeansUsuario mod = new BeansUsuario();
+        
+        JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
+        
+        String nome;
+        String email;
+        String senha;
+        Integer codigo;
+	
+        
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+       
+        nome = (String) jsonObject.get("nome");
+        email = (String)jsonObject.get("email");
+        senha = (String) jsonObject.get("senha");
+        codigo = (Integer) jsonObject.get("codigo");
+        
+        mod.setNome(nome);
+        mod.setEmail(email);
+        mod.setSenha(senha);
+        mod.setCodigo(codigo);
+        u.editar(mod);
+         } catch (org.json.simple.parser.ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    
+    
+    ///////////////////////////ORGAO///////////////////////////////////
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("orgao/inserir")
+    public String insertOrgao(String json){
+    
+        OrgaoDao o = new OrgaoDao();
+        BeansOrgao mod = new BeansOrgao();
+        
+        JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
+        
+        String nome;
+        String senha;
+	
+        
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+     
+        nome = (String) jsonObject.get("nome");
+        senha = (String) jsonObject.get("senha");
+        mod.setNome(nome);
+        mod.setSenha(senha);
+        o.salvar(mod);
+           } catch (org.json.simple.parser.ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("orgao/recuperar")
+    public String recuperarUnico(@QueryParam("id") Long json){
+        OrgaoDao o = new OrgaoDao();
+        BeansOrgao mod = new BeansOrgao();
+        
+        mod.setPesquisarId(json);
+        mod = o.buscarPorId(mod);
+        
+        Gson g = new Gson();
+        return g.toJson(mod);
+    }
+    ///////////////////////////FEED///////////////////////////////////
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("feed/insert")
+    public String insertFeed(String json){
+    
+        FeedDeNoticiaDao f = new FeedDeNoticiaDao();
+        BeansFeedDeNoticia mod = new BeansFeedDeNoticia();
+        
+        JSONObject jsonObject;
+        JSONParser parser = new JSONParser();  
+        
+        String localidade;
+        String descricao;
+        String categoria;
+        Integer idUsuario;
+     
+        try {
+            jsonObject = (JSONObject) parser.parse(json);
+      
+        localidade = (String) jsonObject.get("localidade");
+        descricao = (String) jsonObject.get("descricao");
+        categoria = (String) jsonObject.get("categoria");
+        idUsuario =  (Integer) jsonObject.get("idUsuario");
+        mod.setLocalidade(localidade);
+        mod.setData(LocalDateTime.MAX);
+        mod.setDescricao(descricao);
+        mod.setCategoria(categoria);
+        mod.setIdUsuario(idUsuario);
+        f.salvar(mod);
+          } catch (org.json.simple.parser.ParseException ex) {
+            Logger.getLogger(webService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+   
+}
---
+++ WebServiceMavenDivulgueAqui/src/main/java/teste/main.java
@@ -0,0 +1,401 @@
+// ALTERAR O LINK DE TODAS AS REQUISICOES!
+package teste;
+
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author ander
+ */
+public class main {
+    
+    public static void main(String[] args) {
+         // ALTERAR O LINK DE TODAS AS REQUISICOES!
+    ///////////////////////////ORGAO-INSERIR/////////////////////////////////// 
+     //inserindo com codigo 204
+     /*
+        String nome = "celpe10000";
+        String senha = "123";
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("senha", senha);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-serviceDivulgueAqui/webresources/webService/orgao/inserir");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( inserir orgao)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( inserir orgao) \n" + ex);
+            }
+      */  
+     ///////////////////////////ORGAO-RECUPERAR/////////////////////////////////// 
+     /*
+        String nome ;
+        String senha;
+        long codigo = 2;
+            URL url;
+        try {
+            url = new URL("http://localhost:8084/web-serviceDivulgueAqui/webresources/webService/orgao/recuperar?id="+codigo);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+          
+            int code = connection.getResponseCode();
+            System.out.println(code);
+
+            InputStream inputStrem = connection.getInputStream();
+            BufferedReader br =  new BufferedReader(new InputStreamReader(inputStrem));
+            
+            String a;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((a  = br.readLine()) != null){
+             //a += br.readLine();
+             stringBuilder.append(a);
+            }
+          //  System.out.println(stringBuilder.toString());
+            connection.disconnect();
+            
+             JSONObject jsonObject;
+       
+             JSONParser parser = new JSONParser();  
+      
+            jsonObject = (JSONObject) parser.parse(stringBuilder.toString());
+            
+            codigo = (long) jsonObject.get("codigo");
+            nome = (String) jsonObject.get("nome");
+            senha = (String) jsonObject.get("senha");
+                        
+            System.out.println("o codigo é :" + codigo + " nome : " + nome 
+            + " senha : " + senha);
+
+        } catch (MalformedURLException ex) {
+            JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( recuperar usuario)\n" + ex);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( Recuperar usuario) \n" + ex);
+        } catch (ParseException ex) {
+            JOptionPane.showMessageDialog(null, "erro de ParseException conexao ao rest ( Recuperar usuario) \n" + ex);
+        }
+         */    
+    ///////////////////////////USUARIO-INSERIR///////////////////////////////////         
+    //esta returnando codigo 204 porem inseri
+     /*       
+        String nome = "celpe30000";
+        String email = "celp@celp.com";
+        String senha = "123";
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("email", email);
+        jsonObject.put("senha", senha);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-serviceDivulgueAqui/webresources/webService/usuario/insert");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+      
+    */
+    ///////////////////////////USUARIO-RECUPERAR POR ID ///////////////////////////////////
+    /*
+        String nome ;
+        String email;
+        String senha;
+        long codigo = 2;
+            URL url;
+        try {
+            url = new URL("http://localhost:8084/web-service/webresources/webService/usuario/recuperar?id="+codigo);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+          
+            int code = connection.getResponseCode();
+            System.out.println(code);
+
+            InputStream inputStrem = connection.getInputStream();
+            BufferedReader br =  new BufferedReader(new InputStreamReader(inputStrem));
+            
+            String a;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((a  = br.readLine()) != null){
+             //a += br.readLine();
+             stringBuilder.append(a);
+            }
+          //  System.out.println(stringBuilder.toString());
+            connection.disconnect();
+            
+             JSONObject jsonObject;
+       
+             JSONParser parser = new JSONParser();  
+      
+            jsonObject = (JSONObject) parser.parse(stringBuilder.toString());
+            
+            codigo = (long) jsonObject.get("codigo");
+            nome = (String) jsonObject.get("nome");
+            email = (String) jsonObject.get("email");
+            senha = (String) jsonObject.get("senha");
+                        
+            System.out.println("o codigo é :" + codigo + " nome : " + nome 
+            + " email : " + email + " senha : " + senha);
+
+        } catch (MalformedURLException ex) {
+            JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( recuperar usuario)\n" + ex);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( Recuperar usuario) \n" + ex);
+        } catch (ParseException ex) {
+            JOptionPane.showMessageDialog(null, "erro de ParseException conexao ao rest ( Recuperar usuario) \n" + ex);
+        }
+    */
+    ///////////////////////////USUARIO-RECUPERAR POR NOME ///////////////////////////////////
+    /*
+        String nome = "marcela" ;
+        String email;
+        String senha;
+        long codigo;
+            URL url;
+        try {
+            url = new URL("http://localhost:8084/web-service/webresources/webService/usuario/recuperarPorNome?nome="+nome);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+          
+            int code = connection.getResponseCode();
+            System.out.println(code);
+
+            InputStream inputStrem = connection.getInputStream();
+            BufferedReader br =  new BufferedReader(new InputStreamReader(inputStrem));
+            
+            String a;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((a  = br.readLine()) != null){
+             //a += br.readLine();
+             stringBuilder.append(a);
+            }
+          //  System.out.println(stringBuilder.toString());
+            connection.disconnect();
+            
+             JSONObject jsonObject;
+       
+             JSONParser parser = new JSONParser();  
+      
+            jsonObject = (JSONObject) parser.parse(stringBuilder.toString());
+            
+            codigo = (long) jsonObject.get("codigo");
+            nome = (String) jsonObject.get("nome");
+            email = (String) jsonObject.get("email");
+            senha = (String) jsonObject.get("senha");
+                        
+            System.out.println("O codigo é :" + codigo + " nome : " + nome 
+            + " email : " + email + " senha : " + senha);
+
+        } catch (MalformedURLException ex) {
+            JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( recuperar usuario)\n" + ex);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( Recuperar usuario) \n" + ex);
+        } catch (ParseException ex) {
+            JOptionPane.showMessageDialog(null, "erro de ParseException conexao ao rest ( Recuperar usuario) \n" + ex);
+        }
+    */
+    ///////////////////////////USUARIO-ATUALIZAR ///////////////////////////////////
+    /*
+        String nome = "marcela";
+        String email = "marcela@hotmail.com";
+        String senha = "321";
+        long codigo = 1;
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("email", email);
+        jsonObject.put("senha", senha);
+        jsonObject.put("codigo", codigo);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-serviceDivulgueAqui/webresources/webService/usuario/update");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+    */
+    ///////////////////////////feeddenoticia-INSERIR///////////////////////////////////         
+    //esta returnando codigo 204 porem inseri
+    /*
+        String localidade = "centro4";
+        String descricao = "buraco";
+        String categoria = "infra-estrutura";
+        long idUsuario = 1;
+        
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("localidade", localidade);
+        jsonObject.put("descricao", descricao);
+        jsonObject.put("categoria", categoria);
+        jsonObject.put("idUsuario", idUsuario);           
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/web-serviceDivulgueAqui/webresources/webService/feed/insert");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = connection.getOutputStream();
+            os.write(Json.getBytes("UTF-8"));
+            os.flush();
+
+            int code = connection.getResponseCode();
+            System.out.println(code + " - " + Json);
+
+            os.close();
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+      */      
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    }
+    
+}
